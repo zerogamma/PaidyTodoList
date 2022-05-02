2ef08225ce0b128385615df925f7770ea56f0795
Jin Park <jin@projectcor.com>
2022-05-01 23:55:04 -0300

    test setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Security from "./containers/Security";
-import * as LocalAuthentication from "expo-local-authentication";
-import { View, Text } from "react-native";
 
 export default function App() {
   return <Security />;
